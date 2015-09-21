030fcb41af98fe4259c5cad3a59f5ee6c66ba008
Mark Sunghyuk Park <sunghyukpark0408@gmail.com>
2015-09-21 03:15:32 -0700

    delete comment

--- routes.js
+++ routes.js
@@ -18,15 +18,9 @@ module.exports = function(app, passport) {
       res.redirect('/');
     });
 
-    // =====================================
-    // GOOGLE ROUTES =======================
-    // =====================================
-    // send to google to do the authentication
-    // profile gets us their basic information including their name
-    // email gets their emails
+
     app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-    // the callback after google has authenticated the user
     app.get('/auth/google/callback',
       passport.authenticate('google', {
         successRedirect : '/profile',
@@ -36,13 +30,8 @@ module.exports = function(app, passport) {
 
   };
 
-// route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-
-  // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
     return next();
-
-  // if they aren't redirect them to the home page
   res.redirect('/');
 }

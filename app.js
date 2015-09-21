7cd54e2345c3c7035bb1ae017e96a4251f762ac1
Mark Sunghyuk Park <sunghyukpark0408@gmail.com>
2015-09-20 23:56:31 -0700

    configure routes, add login,register routes and includes

--- app.js
+++ app.js
@@ -5,11 +5,41 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 
-var app = express();
+var app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// configure express app
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// cofngure session,passport for user authentication
+app.use(express.session({ secret: process.env('SESSION_SECRET') }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+
+// configure routes
+var routes = require('./routes/index');
+var login = require('./routes/login');
+var register = require('./routes/register');
+
+app.use('/', routes);
+app.use('/login', login);
+app.use('register', register);
+
+
+
+
+
 
 server.listen(4000);
 
@@ -43,55 +73,6 @@ io.sockets.on("connection", function(socket){
 });
 
 
-
-
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
 module.exports = app;

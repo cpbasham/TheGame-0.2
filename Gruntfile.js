6fd55e9e155e9fb781cb63982251c0a0a1847b73
Arturo Perez <VrturoPerez@gmail.com>
2015-09-23 16:24:27 -0700

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 3000,
+        port: 8080,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },

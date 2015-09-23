98d2ca2b15fbac2b4bf3d8d1bcd306d598cb568f
Philip Yoo <philipyoo10@gmail.com>
2015-09-23 13:01:02 -0700

    Merge view files and changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(express,passport){
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  var router = express.Router();
 
-module.exports = router;
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express', user: req.user });
+  });
+
+  router.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+  });
+
+  router.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+  router.get('/auth/google/callback',
+    passport.authenticate('google', {
+      successRedirect : '/thegame',
+      failureRedirect : '/'
+    })
+  );
+
+  router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+  router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      successRedirect : '/thegame',
+      failureRedirect : '/'
+    })
+  );
+
+  router.get('/thegame', function(req,res){
+    res.render('game', {title: 'DA GAME'});
+  });
+
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+      return next();
+    res.redirect('/');
+  }
+  return router;
+}
+
+
+

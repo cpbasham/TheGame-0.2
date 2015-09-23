5b8a1487f0ba88b7668dd4c050da32f0dbc72c58
Mark Sunghyuk Park <sunghyukpark0408@gmail.com>
2015-09-23 15:26:18 -0700

    fix view fils, set running grunt to false

--- public/dist/js/game.js
+++ public/dist/js/game.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var runningGrunt = true;
+var runningGrunt = false;
 
 var Player = require("../prefabs/player.js");
 var Bullet = require("../prefabs/bullet.js");
@@ -146,7 +146,7 @@ window.onload = function () {
   game.state.add('menu', require('./states/menu'));
   game.state.add('play', require('./states/play'));
   game.state.add('preload', require('./states/preload'));
-  
+
 
   game.state.start('boot');
 };
